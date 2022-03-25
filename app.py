0ac6ef917c419f71e09fc31496c52e568c308be2
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2022-03-25 17:16:16 +0700

    test

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/test", methods=['GET'])
+def hello():
+  q = request.args.get('q')
+  print(q)
+  return { "message": "Hello!" }, 201
+
+if __name__ == "__main__":
+    app.run()
